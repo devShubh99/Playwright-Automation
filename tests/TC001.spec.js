5e50561398afd98b24155756083d46ca28a0d4a4
Shubham Anand <ashubham32@gmail.com>
2025-08-18 17:34:33 +0530

    Added a Test Case

--- tests/TC001.spec.js
+++ tests/TC001.spec.js
@@ -4,7 +4,7 @@ test('Home Page',async({page})=>{
 
 await page.goto('https://demoblaze.com/');
 const pageTitle =  page.title();
-console.log{'Page title is', pageTitle};
+console.log;{'Page title is', pageTitle};
 
 await expect(page).toHaveTitle('STORE');
 await expect(page).toHaveURL('https://demoblaze.com/');
