212c36e83a2fffbc5af4da5147014aa39f9094cd
Shubham Anand <ashubham32@gmail.com>
2025-09-25 08:20:21 +0000

    Added new validations in TC010

--- tests/TC010.spec.js
+++ tests/TC010.spec.js
@@ -4,8 +4,10 @@ test ("Revising", async({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com/")
     await page.locator("//input[@id='name']").fill("Shubham Anand")
     await page.locator("//input[@id='email']").fill("test@email.com")
-    
-
-
-
+    await page.locator("(//input[@id='phone'])[1]").fill("123456789")
+    await page.locator("//textarea[@id='textarea']").fill("Test Address")
+    const Gender_M = await page.locator("(//input[@id='male'])[1]")
+    await expect(Gender_M).not.toBeChecked()
+    const Gender_F = await page.locator("//input[@id='female']")
+    await expect(Gender_F).not.toBeChecked()
 })
